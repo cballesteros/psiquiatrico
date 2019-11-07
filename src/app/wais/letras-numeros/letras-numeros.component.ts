4726f8a14b906c79c54896b850987cf8a466af6a
Daniel0205 <daniel.alejandro.diaz@correounivalle.edu.co>
2019-11-07 00:51:15 -0500

    comprension basico agregado

--- src/app/wais/letras-numeros/letras-numeros.component.ts
+++ src/app/wais/letras-numeros/letras-numeros.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, HostListener } from '@angular/core';
+import { Component, OnInit, HostListener, ChangeDetectorRef  } from '@angular/core';
 
 @Component({
   selector: 'app-letras-numeros',
@@ -30,6 +30,10 @@ export class LetrasNumerosComponent implements OnInit {
 
   items=[];
 
+  respuesta=[];
+
+  respuestas=[]
+
   resultados:number[] = new Array(30).fill(0);
 
   cambiarPrueba(key){
@@ -53,9 +57,12 @@ export class LetrasNumerosComponent implements OnInit {
       }
     }
     else if(this.itemNumber===10)this.estado='resultados'
+    this.cdRef.detectChanges();
   }
 
-  constructor() {
+
+
+  constructor(private cdRef:ChangeDetectorRef) {
     
     this.items.push([['2','B'],['D','1'],['4','C']])
 
@@ -76,7 +83,48 @@ export class LetrasNumerosComponent implements OnInit {
     this.items.push([['R','7','V','4','T','8','F'],['9','X','2','J','3','N','7'],['M','1','Q','8','R','4','D']])
 
     this.items.push([['6','P','7','S','2','N','9','A'],['E','1','R','9','X','4','K','3'],['7','M','2','T','6','F','9','A']])
+   
+    this.respuesta.push([['2','B'],['D','1'],['4','C']])
+
+    this.respuesta.push([['E','5'],['3','A'],['C','1']])
+ 
+    this.respuesta.push([['5','C','A'],['F','E','1'],['3','2','A']])
+
+    this.respuesta.push([['1','G','7'],['H','9','4'],['3','Q','7']])
+
+    this.respuesta.push([['Z','8','N'],['M','6','E'],['P','2','N']])
+
+    this.respuesta.push([['V','1','J','5'],['7','X','4','G'],['S','9','T','6']])
+
+    this.respuesta.push([['8','E','6','F','1'],['K','4','C','2','S'],['5','Q','3','H','6']])
+
+    this.respuesta.push([['M','4','P','7','R','2'],['6','N','9','J','2','S'],['E','6','H','5','F','3']])
+
+    this.respuesta.push([['R','7','V','4','T','8','F'],['9','X','2','J','3','N','7'],['M','1','Q','8','R','4','D']])
+
+    this.respuesta.push([['6','P','7','S','2','N','9','A'],['E','1','R','9','X','4','K','3'],['7','M','2','T','6','F','9','A']])
+
+    this.respuestas.push([['2','B'],['D','1'],['4','C']])
+
+    this.respuestas.push([['E','5'],['3','A'],['C','1']])
+ 
+    this.respuestas.push([['A','C','5'],['E','F','1'],['A','2','3']])
+
+    this.respuestas.push([['G','1','7'],['H','4','9'],['Q','3','7']])
+
+    this.respuestas.push([['N','Z','8'],['E','M','6'],['N','P','2']])
+
+    this.respuestas.push([['J','V','1','5'],['G','X','4','7'],['S','T','6','9']])
+
+    this.respuestas.push([['E','F','1','6','8'],['C','K','S','2','4'],['H','Q','3','5','6']])
+
+    this.respuestas.push([['M','P','R','2','4','7'],['J','N','S','2','6','9'],['E','F','H','3','5','6']])
+
+    this.respuestas.push([['F','R','T','V','4','7','8'],['J','N','X','2','3','7','9'],['D','M','Q','R','1','4','8']])
+
+    this.respuestas.push([['A','N','P','S','2','6','7','9'],['E','K','R','X','1','3','4','9'],['A','F','M','T','2','6','7','9']])
     
+
   }
 
   getResultado():number {
